a0758d96d19d4576fdbc178cf7ebbf956d3ba711
Kasey P. Spry <92997499+KaseyPSpry@users.noreply.github.com>
2023-01-30 18:06:37 -0500

    Addition of data visualization

--- runScript_project1.R
+++ runScript_project1.R
@@ -7,3 +7,28 @@ data <- read.csv(
   stringsAsFactors = TRUE
 )
 
+#Visualization of dependent variables
+hist(data$ANXIETY)
+hist(data$MEMORY)
+
+# Visualization of Anxiety variable by independent variables (sex, dose, and pre v post)
+par(mfrow = c(4,4)) #plots in multiframe mode
+for(sex in c("MALE", "FEMALE")){ 
+  for (prepost in c("PRE","POST")){
+    for(dose in c("SHAM","LOW","MED","HIGH")){
+      thegroup <- which((sex==data$SEX)&(dose==data$DOSE)&(prepost==data$PREPOST))
+      hist(data$ANXIETY[thegroup],xlab = paste(sex,",", dose,",", prepost),main = "Anxiety")
+    }
+  }
+}
+
+# Visualization of Memory variable by independent variables (sex, dose, and pre v post)
+par(mfrow = c(4,4)) #plots in multiframe mode
+for(sex in c("MALE", "FEMALE")){ 
+  for (prepost in c("PRE","POST")){
+    for(dose in c("SHAM","LOW","MED","HIGH")){
+      thegroup <- which((sex==data$SEX)&(dose==data$DOSE)&(prepost==data$PREPOST))
+      hist(data$MEMORY[thegroup],xlab = paste(sex,",", dose,",", prepost),main = "Memory")
+    }
+  }
+}
