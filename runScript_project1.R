24c57dbcbeb34b60bcba72e3f429ebedd742008c
Kasey P. Spry <92997499+KaseyPSpry@users.noreply.github.com>
2023-02-20 15:07:53 -0500

    Addition of logit model

--- runScript_project1.R
+++ runScript_project1.R
@@ -1,4 +1,5 @@
-library(ggplot2)
+library("ggplot2")
+library("gtools")
 
 
 datlong <- read.table('/Users/kaseyspry/Documents/Wake Forest/5. NEUR PhD Classes/1. NEUR 751/NEUR751/behavpharma_v1.csv', 
@@ -14,15 +15,20 @@ dat <- reshape(datlong,
 dat$DOSE <- factor(dat$DOSE,
                    levels = c("SHAM", "LOW", "MED", "HIGH")
                    )
+dat$ANXIETY.PRE.LOGIT <- logit(dat$ANXIETY.PRE)
+dat$ANXIETY.POST.LOGIT <- logit(dat$ANXIETY.POST)
 
-model.memory <- lm(data = dat,
-            MEMORY.POST~SEX + DOSE + SEX*DOSE + MEMORY.PRE)
-model.anxiety <- lm(data = dat,
-                    ANXIETY.POST~ANXIETY.PRE + SEX + DOSE + SEX*DOSE)
+model.anxiety <- lm(data = dat, ANXIETY.POST.LOGIT~ANXIETY.PRE.LOGIT + SEX + DOSE +SEX:DOSE)
 print(summary(model.anxiety))
+print(anova(model.anxiety))
+# model.memory <- lm(data = dat,
+#             MEMORY.POST~SEX + DOSE + SEX*DOSE + MEMORY.PRE)
+# model.anxiety <- lm(data = dat,
+#                     ANXIETY.POST~ANXIETY.PRE + SEX + DOSE + SEX*DOSE)
+#print(summary(model.anxiety))
 
 
-g <- ggplot( data = dat, 
-             aes(x = SEX, y = ANXIETY.POST))
-g <- g + geom_violin()
-print(g)
+# g <- ggplot( data = dat, 
+#              aes(x = DOSE, y = ANXIETY.POST.LOGIT, fill = SEX))
+# g <- g + geom_violin()
+# print(g)
