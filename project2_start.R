a8dcf9701cdb85dd0a6e132d5eb30464ff813918
KaseyPSpry <kpotts@wakehealth.edu>
2023-04-12 16:47:15 -0400

    Setting up problem 2

---
+++ project2_start.R
@@ -0,0 +1,29 @@
+library("ggplot2")
+library("Rmisc")
+
+dat <- read.table(
+  file = "/Volumes/home5$/kpotts/11. NEUR PhD Classes/NEUR 751/NEUR 751/livedie_v1.csv",
+  header = TRUE,
+  sep = ","
+) 
+
+dat$LIVED <- as.factor(dat$LIVED)
+
+g1 <- ggplot(
+  data = dat
+) +
+  geom_point(aes(x = VARS.1, y = VARS.2, col = LIVED))
+g2 <- ggplot(
+  data = dat
+) +
+  geom_point(aes(x = VARS.2, y = VARS.3, col = LIVED))
+g3 <- ggplot(
+  data = dat
+) +
+  geom_point(aes(x = VARS.3, y = VARS.4, col = LIVED))
+g4 <- ggplot(
+  data = dat
+) +
+  geom_point(aes(x = VARS.4, y = VARS.5, col = LIVED))
+
+multiplot(g1, g2, g3, g4, cols = 2)
