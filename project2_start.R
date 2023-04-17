2e92fbde81df96fb3aa355c2a7084862c48ece42
KaseyPSpry <kpotts@wakehealth.edu>
2023-04-16 22:07:56 -0400

    Project 2 scripts

--- project2_start.R
+++ project2_start.R
@@ -6,24 +6,28 @@ dat <- read.table(
   header = TRUE,
   sep = ","
 ) 
-
 dat$LIVED <- as.factor(dat$LIVED)
+dat[,2:13] <- scale(dat[,2:13])
 
-g1 <- ggplot(
-  data = dat
-) +
-  geom_point(aes(x = VARS.1, y = VARS.2, col = LIVED))
-g2 <- ggplot(
-  data = dat
-) +
-  geom_point(aes(x = VARS.2, y = VARS.3, col = LIVED))
-g3 <- ggplot(
-  data = dat
-) +
-  geom_point(aes(x = VARS.3, y = VARS.4, col = LIVED))
-g4 <- ggplot(
-  data = dat
-) +
-  geom_point(aes(x = VARS.4, y = VARS.5, col = LIVED))
+lived <- which(dat$LIVED == "1")
+died <- which(dat$LIVED == "0")
+pca <- princomp(dat[,2:13])
 
-multiplot(g1, g2, g3, g4, cols = 2)
+# g1 <- ggplot(
+#   data = dat
+# ) +
+#   geom_point(aes(x = VARS.1, y = VARS.2, col = LIVED))
+# g2 <- ggplot(
+#   data = dat
+# ) +
+#   geom_point(aes(x = VARS.2, y = VARS.3, col = LIVED))
+# g3 <- ggplot(
+#   data = dat
+# ) +
+#   geom_point(aes(x = VARS.3, y = VARS.4, col = LIVED))
+# g4 <- ggplot(
+#   data = dat
+# ) +
+#   geom_point(aes(x = VARS.4, y = VARS.5, col = LIVED))
+# 
+# multiplot(g1, g2, g3, g4, cols = 2)
---
+++ project2_v03.R
@@ -0,0 +1,34 @@
+library("ggplot2")
+library("Rmisc")
+library("caret")
+library("MASS")
+library("heplots")
+
+dat <- read.table(
+  file = "/Volumes/kpotts/11. NEUR PhD Classes/NEUR 751/NEUR 751/livedie_v1.csv",
+  header = TRUE,
+  sep = ","
+) 
+dat$LIVED <- as.factor(dat$LIVED)
+dat[,2:13] <- scale(dat[,2:13])
+
+lived <- which(dat$LIVED == "1")
+died <- which(dat$LIVED == "0")
+
+livingcenter <- colMeans(dat[lived,2:13])
+deadcenter <- colMeans(dat[died,2:13])
+living_cov <- cov(dat[lived,2:13])
+dead_cov <- cov(dat[died,2:13])
+
+distance_to_live <- mahalanobis(dat[,2:13], livingcenter, living_cov)
+distance_to_die <- mahalanobis(dat[,2:13], deadcenter, dead_cov)
+
+doyoulive <- distance_to_live < distance_to_die
+
+dat$mpredictions <- as.factor(as.numeric(doyoulive))
+CM <- confusionMatrix(data = dat$mpredictions, reference = dat[,1])
+
+boxmresult <- boxM(dat[,2:13],dat[,1])
+
+LDA <- lda(data = dat,LIVED ~., CV = TRUE)
+CM <- confusionMatrix(data = LDA$class, reference = dat[,1])
